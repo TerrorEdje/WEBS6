90aa01b3c6150c780f4aaa1efac0ba57bd68a4dd
Edwin Hattink <edwinhattink@me.com>
2015-06-22 18:48:21 +0200

    Login/logout button werken

--- app/js/login/LoginController.js
+++ app/js/login/LoginController.js
@@ -5,15 +5,22 @@ angular.module('webs6').controller('LoginController', function(UserService, $htt
       if (username != undefined && token != undefined) {
             $cookies.put('username',username);
             $cookies.put('token',token);
-            console.log(token);
-            console.log($cookies.get('username'));
-            UserService.name = username;
-            UserService.token = token;
-            //$window.location.href = '/#/list/1';
+            $window.location.href = '/';
+      }
+
+      if ($cookies.get('username') != undefined && $cookies.get('token') != undefined) {
+            $scope.user = { _id: $cookies.get('username'), username: $cookies.get('username'), token: $cookies.get('token') };
+            $cookies.put('user', JSON.stringify($scope.user));
       }
 
       $scope.launchLogin = function () {
             $window.location.href = 'http://mahjongmayhem.herokuapp.com/auth/avans?callbackUrl=http://localhost:8080/%23/login/callback';
       }
-      //$scope.launchLogin();
+
+      $scope.logout = function() {
+            $cookies.remove('user');
+            $cookies.remove('username');
+            $cookies.remove('token');
+            $window.location.href = '/';
+      }
 });
